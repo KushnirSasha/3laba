e69f57fe1b912cc64e9e756a84cbe929f5303ff0
Кушнір Олександр <55057488+KushnirSasha@users.noreply.github.com>
2021-06-09 20:29:06 +0300

    Add files via upload

---
+++ 3 laba/public/front.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint space-before-blocks: ["error", "never"]*/
+
+/**
+ *
+ * @param {form} form_data
+ */
+
+function check(form_data){
+  if (form_data.main_input.value != ''){
+    form_data.submit();
+  }
+}
+
